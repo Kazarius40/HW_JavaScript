f367ab2439d9172430705b3ca8b1620da0b990c2
Kazarius40 <roman.dotsenko.de@gmail.com>
2024-11-06 15:27:03 +0100

    Виконав частину ДЗ

---
+++ HW/HW_lesson2/HW1_#67kfznmiMl/main.js
@@ -0,0 +1,11 @@
+let a = [128, true, -58, 'object', false, 78, 1929, 'etwas', 98, 'abrakadabra'];
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+console.log(a[6]);
+console.log(a[7]);
+console.log(a[8]);
+console.log(a[9]);
---
+++ HW/HW_lesson2/HW2_#LARqoUj5I/main.js
@@ -0,0 +1,20 @@
+let book1 = {
+    title: '2000 лье под водой',
+    pageCount: 198,
+    genre: 'Фантастика'
+}
+console.log(book1);
+
+let book2 = {
+    title: 'Три мушкетера',
+    pageCount: 144,
+    genre: 'Роман'
+}
+console.log(book2);
+
+let book3 = {
+    title: 'Все о животных',
+    pageCount: 234,
+    genre: 'Энциклопедия'
+}
+console.log(book3);
